721bf520495ca68bc98b0a62590a34ea1c240397
Mike Simpson <mike.simpson@scimedsolutions.com>
2014-07-18 21:43:33 -0400

    Louie, I think this is the beginning of a beautiful friendship

---
+++ init.el
@@ -0,0 +1,14 @@
+;;; Initialize --- Provides local customizations
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'org)
+(org-babel-load-file "~/.emacs.d/readme.org")
+
+;(require 'rss-config)
+;(require 'social-config)
+
+(provide 'personal-init)
+;;; personal-init ends here
